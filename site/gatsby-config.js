5ab6f17aabcf188645b985711bec559c77e6f8a1
Christopher Biscardi <biscarch@sketcht.com>
2019-03-19 01:23:44 -0700

    init a gatsby site

---
+++ site/gatsby-config.js
@@ -0,0 +1,39 @@
+module.exports = {
+  siteMetadata: {
+    title: `Main Actor`,
+    description: `Package documentation for ponylang`,
+    author: `Chris Biscardi`,
+  },
+  plugins: [
+    `gatsby-plugin-react-helmet`,
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `images`,
+        path: `${__dirname}/src/images`,
+      },
+    },
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `docs`,
+        path: `../docs-raw-md`,
+      },
+    },
+    `gatsby-transformer-sharp`,
+    `gatsby-plugin-sharp`,
+    `gatsby-transformer-remark`,
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `ponylang-main-actor`,
+        short_name: `main-actor`,
+        start_url: `/`,
+        background_color: `#663399`,
+        theme_color: `#663399`,
+        display: `minimal-ui`,
+        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+      },
+    },
+  ],
+}
