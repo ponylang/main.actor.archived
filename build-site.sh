bfa028b7a106923e78922efd55833e0566670d03
Sean T. Allen <sean@seantallen.com>
2019-09-01 16:03:06 -0400

    Work around a Netlify bad cache issue

--- build-site.sh
+++ build-site.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+# This is temporary and should be removed once the non-existent yet
+# there it is `docs` directory is removed. I think its a Netlify caching
+# issue. I've reached out to them for support.
+echo "Remove 'ghost' directory..."
+rm -rf docs
+
 echo "Fetching latest documentation markdown..."
 git clone --depth 1 \
   https://github.com/ponylang/main.actor-package-markdown.git \
